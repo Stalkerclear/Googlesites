4f3cc72f68a7d22e40c496f07d2ef33610d61b2e
Stalkerclear <stalkernois64@gmail.com>
2025-05-18 20:27:30 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,16 @@
 
+// pages/index.js
+export default function Index() {
+  return (
+    <div style={{ color: "#0f0", background: "#000", fontFamily: "monospace", padding: "2rem" }}>
+      <h1>Redirecionando...</h1>
+      <script dangerouslySetInnerHTML={{
+        __html: `
+          setTimeout(() => {
+            window.location.href = '/painel';
+          }, 10);
+        `
+      }} />
+    </div>
+  );
+}
